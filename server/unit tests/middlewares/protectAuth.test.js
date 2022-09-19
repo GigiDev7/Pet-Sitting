f95c23aacab021d5109dcc06a4b56354fba7e445
GigiDev7 <gigivadachkoria@gmail.com>
2022-09-19 13:58:27 +0400

    added succes case for protect auth unit test

--- server/unit tests/middlewares/protectAuth.test.js
+++ server/unit tests/middlewares/protectAuth.test.js
@@ -6,20 +6,34 @@ const { errorHandler } = require("../../middlewares/errorHandler");
 jest.mock("jsonwebtoken");
 jest.mock("../../models/userSchema");
 
-const req = {};
-
-const res = {
-  status: jest.fn().mockReturnThis(),
-  json: jest.fn((x) => x),
+const res = {};
+const reqError = {};
+const reqSuccess = {
+  headers: {
+    authorization: "Bearer token",
+  },
 };
 
 describe("protect auth middleware", () => {
   describe("auth ERROR", () => {
     it("should return authorization error", () => {
-      expect(() => protectAuth(req, res)).toThrowError({
+      expect(() => protectAuth(reqError, res)).toThrowError({
         name: "Authorization Error",
         message: "Authorization failed",
       });
+    });
+  });
+
+  describe("auth SUCCESS", () => {
+    it("should not throw an error", () => {
+      User.findById.mockResolvedValue({
+        email: "email",
+        password: "password",
+      });
+      jwt.verify.mockReturnValue({ id: "userId" });
+      const next = jest.fn();
+
+      expect(() => protectAuth(reqSuccess, res)).not.toThrow();
     });
   });
 });
