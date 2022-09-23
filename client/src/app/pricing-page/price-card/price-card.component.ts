d49e30e1ce1ee1c9912c26ef47e1e1e9cd6a426e
GigiDev7 <gigivadachkoria@gmail.com>
2022-09-23 20:33:32 +0400

    more ui for pricing page

--- client/src/app/pricing-page/price-card/price-card.component.ts
+++ client/src/app/pricing-page/price-card/price-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-price-card',
   templateUrl: './price-card.component.html',
-  styleUrls: ['./price-card.component.css']
+  styleUrls: ['./price-card.component.css'],
 })
 export class PriceCardComponent implements OnInit {
+  @Input() type!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
