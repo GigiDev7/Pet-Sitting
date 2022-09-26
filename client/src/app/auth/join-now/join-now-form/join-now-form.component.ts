41b5ccc9ffff2daf0a7cd0036f3c0e82488f72e9
GigiDev7 <gigivadachkoria@gmail.com>
2022-09-26 18:17:36 +0400

    added errors on form, UI

--- client/src/app/auth/join-now/join-now-form/join-now-form.component.ts
+++ client/src/app/auth/join-now/join-now-form/join-now-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth.service';
 
@@ -8,6 +9,18 @@ import { AuthService } from '../../auth.service';
   styleUrls: ['./join-now-form.component.css'],
 })
 export class JoinNowFormComponent implements OnInit, OnDestroy {
+  public maxDateRestriction!: string;
+
+  public registerForm: FormGroup = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    dateOfBirth: new FormControl('', [Validators.required]),
+    location: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
+
   onCloseFormClick() {
     this.authService.closeJoinForm();
   }
@@ -17,6 +30,8 @@ export class JoinNowFormComponent implements OnInit, OnDestroy {
     this.router.navigate(['login']);
   }
 
+  onRegisterFormSubmit() {}
+
   constructor(
     private renderer: Renderer2,
     private authService: AuthService,
@@ -25,6 +40,19 @@ export class JoinNowFormComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.renderer.setStyle(document.body, 'overflow-y', 'hidden');
+
+    const yearDifference = new Date().getFullYear() - 18;
+    let extraDayYears = 0;
+    for (let i = yearDifference; i <= new Date().getFullYear(); i++) {
+      if (i % 4 === 0) extraDayYears++;
+    }
+    const days = extraDayYears * 366 + (18 - extraDayYears) * 365;
+    const maxDate = new Date(new Date().getTime() - days * 24 * 60 * 60 * 1000);
+    this.maxDateRestriction = `${maxDate.getFullYear()}-${
+      maxDate.getMonth().toString().length === 1
+        ? '0' + maxDate.getMonth()
+        : maxDate.getMonth()
+    }-${maxDate.getDate()}`;
   }
 
   ngOnDestroy(): void {
