ed1031076027ff1d789a060e142fabe8659af085
GigiDev7 <gigivadachkoria@gmail.com>
2022-10-10 18:51:38 +0400

    added typeahead for searching

--- client/src/app/sitters-page/sitters-page.component.ts
+++ client/src/app/sitters-page/sitters-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CountryService, ICountry } from '../auth/country.service';
 import { IUser, SitterService } from '../sitter.services';
 
 @Component({
@@ -12,18 +13,39 @@ export class SittersPageComponent implements OnInit {
   public petVariants: string[] = ['Dogs', 'Cats', 'Birds', 'Fish', 'Horses'];
   public selectedPets!: string[];
   public isSelectedPetBoxShown: boolean = false;
+  public countries: ICountry[] = [];
+  public countrySearch: string = '';
 
   toggleSelectedPetBox() {
     this.isSelectedPetBoxShown = !this.isSelectedPetBoxShown;
   }
 
+  onLocationChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+
+    this.countries = this.countryService.countries.filter((el) =>
+      el?.country?.toLowerCase().includes(target.value.toLowerCase())
+    );
+  }
+
+  onLocationClick(event: string) {
+    this.countrySearch = event;
+    this.countries = [];
+  }
+
   constructor(
     private sitterService: SitterService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private countryService: CountryService
   ) {}
 
   ngOnInit(): void {
     this.selectedPets = this.route.snapshot.queryParams['pets'].split(',');
+
+    if (!this.countryService.countries.length) {
+      this.countryService.getCountries().subscribe();
+    }
+
     this.sitterService.sitters.subscribe({
       next: (val) => {
         this.sitters = val;
