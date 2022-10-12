4f5afab8dd73dabb969fbf62c072cac32695f40f
GigiDev7 <gigivadachkoria@gmail.com>
2022-10-12 14:11:30 +0400

    added text area and button for adding coment

--- client/src/app/single-sitter-page/comment-box/comment-box.component.ts
+++ client/src/app/single-sitter-page/comment-box/comment-box.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IUser } from 'src/app/sitter.services';
 
 @Component({
   selector: 'app-comment-box',
   templateUrl: './comment-box.component.html',
-  styleUrls: ['./comment-box.component.css']
+  styleUrls: ['./comment-box.component.css'],
 })
 export class CommentBoxComponent implements OnInit {
+  @Input() sitter!: IUser;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
